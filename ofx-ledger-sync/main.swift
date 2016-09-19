2d6ae45c38d311cd7ba697708805dd79c70304be
Stef Horner <shorner@golin.com>
2016-09-19 10:22:45 +0100

    Initial Commit

---
+++ ofx-ledger-sync/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ofx-ledger-sync
+//
+//  Created by Horner, Stefan (LDN-ARC) on 19/09/2016.
+//  Copyright © 2016 tedslittlerobot. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
